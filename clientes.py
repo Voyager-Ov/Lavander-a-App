9e4e346105b57c5d8d9388bf5ff16601a7a8a568
Voyager-Ov <octavio.velo2022@gmail.com>
2024-11-24 23:58:01 -0300

    locuras

--- clientes.py
+++ clientes.py
@@ -1,7 +1,7 @@
-import funciones_Add
-
-
-Archivo_de_Clientes = "Clientes.csv"
+import funciones_Add as fa
+import pickle
+import os.path
+Archivo_de_Clientes = "Clientes.pkl"
 
 
 class Clientes:    # Creo la clase Clientes para que tenga los atributos vistos
@@ -37,16 +37,60 @@ def crear_Cliente():       # defino la funcion para crear al cliente
     telefono = input("Telefono: ")
     email = input("Email: ")
     direccion = input("Direccion: ")
-    subscripcion = input("Subscripcion: ")
-    pedidos = "falta encontrar funcion para la lista de pedidos"
+    subscripcion = "falta encrontar lka funcion para las subscripciuones"
+    pedidos = "falta encrontar lka funcion para las pedidos"
     # Instancio el objeto con los parametros que nos pasan
     cliente = Clientes(dni, nombre, apellido, telefono, email, direccion, pedidos, subscripcion)
     # uso la funcion de Funciones add para cargar el objeto cliente al archivo de Clientes
-    Funciones_Add.Cargar_objeto_al_Archivo(cliente, Archivo_de_Clientes)
+    print("Los datos del clinete son los siguientes: ")
+    print(cliente)
+    cliente_mod = cambiar_Datos_Cliente(cliente)
+    fa.Cargar_objeto_al_Archivo(cliente_mod, Archivo_de_Clientes)
+    return cliente_mod
 
 
-def mostrar_Clientes(cliente):
+def mostrar_Clientes():
     print("          ///CLIENTES////")
-    for i in cliente:
-        print(i)
+    if os.path.exists(Archivo_de_Clientes):
+        arc = open(Archivo_de_Clientes, "rb")
+        t = os.path.getsize(Archivo_de_Clientes)
+        while arc.tell() < t:
+            cliente = pickle.load(arc)
+            print(cliente)
+        arc.close
 
+
+
+def buscar_cliente(dni):
+    if os.path.exists(Archivo_de_Clientes):
+        arc = open(Archivo_de_Clientes, "rb")
+        clientes = pickle.load(arc)
+        for i in clientes:
+            if i.dni == dni:
+                return i
+            return None
+        
+
+def cambiar_Datos_Cliente(cliente):
+    print("DESEA CAMBIAR LOS DATOS DEL CLIENTE???")
+    print("1. Si")
+    print("2. No")
+    eleccion = input("Ingrese la opcion que desee: ")
+    if eleccion == "1":
+        print("Para cambiar los datos del cliente ingrese complete los siguientes campos: ")
+        dni = input("DNI: ")
+        nombre = input("Nombre: ")
+        apellido = input("Apellido: ")
+        telefono = input("Telefono: ")
+        email = input("Email: ")
+        direccion = input("Direccion: ")
+        subscripcion = "falta encrontar lka funcion para las subscripciuones"
+        pedidos = "falta encontrar funcion para la lista de pedidos"
+        # Instancio el objeto con los parametros que nos pasan
+        cliente = Clientes(dni, nombre, apellido, telefono, email, direccion, pedidos
+                           , subscripcion)
+        return cliente
+    else:
+        return cliente
+
+mostrar_Clientes()
--- funciones_Add.py
+++ funciones_Add.py
@@ -1,6 +1,6 @@
 import os.path
 import pickle
-
+Archivo_de_Clientes = "Clientes.pkl"
 
 def Cargar_objeto_al_Archivo(objeto, archivo):
     """
@@ -10,29 +10,66 @@ def Cargar_objeto_al_Archivo(objeto, archivo):
     :param archivo:
     """
     if os.path.exists(archivo):
-        arc = open(archivo, 'a')
+        arc = open(archivo, 'ab')
         pickle.dump(objeto, arc)
         arc.close()
     else:
-        arch = open(archivo, 'w')
+        arch = open(archivo, 'wb')
         pickle.dump(objeto, arch)
         arch.close()
 
 
-def ordenar_Archivo(archivo, parametro):
+def ordenar_archivo(archivo, parametro):
     """
-    ordena a los objetos del archivo por el parametro de menor a mayor
-    :param archivo:
-    :param parametro:
-    :return: Archivo ordenado por el parametro
+    Ordena a los objetos del archivo por el parámetro de menor a mayor.
+    
+    :param archivo: nombre del archivo que contiene los objetos.
+    :param parametro: El atributo por el cual se ordenarán los objetos.
+    :return: Archivo ordenado por el parámetro.
     """
     if os.path.exists(archivo):
-        arc = open(archivo, 'r')
+        with open(archivo, 'rb') as arc:  # Abrir en modo lectura 
+            try:
+                clientes = pickle.load(arc)
+            except EOFError:
+                return  # Si el archivo está vacío, no hacemos nada
+
+        # Ordenar los clientes
+        clientes_ordenados = sorted(clientes, key=lambda persona: getattr(persona, parametro))
+
+        # Guardar los clientes ordenados de nuevo en el archivo
+        with open(archivo, 'wb') as arc:  # Abrir en modo escritura 
+            pickle.dump(clientes_ordenados, arc)
+
+
+
+
+def Buscador_Coincidencias_en_Archivo_mio(parametro, archivo, coincidencia):
+    if os.path.exists(archivo):
+        arc = open(archivo, "rb")
         clientes = pickle.load(arc)
-        arc.close()
-        clientes_ordenados = sorted(clientes, key=lambda persona: persona.parametro)
-        
+        for i in clientes:
+            if getattr(i, parametro) == coincidencia:
+                return True  # Se encontró al menos una coincidencia
+        return False  # No se encontraron coincidencias
+    
 
-
-def Buscador_Coincidencias_en_Archivo(objeto, parametro, archivo):
-    pass
+def Buscador_Coincidencias_en_Archivo_blackbox(parametro, archivo, coincidencia):
+    """
+    Busca coincidencias en el archivo para el parámetro dado y la coincidencia especificada.
+    Retorna True si se encuentra al menos un objeto, False si no se encuentran coincidencias.
+    :param parametro: El atributo del objeto en el que se va a buscar la coincidencia.
+    :param archivo: El archivo donde se almacenan los objetos.
+    :param coincidencia: El valor que se está buscando.
+    :return: True si se encuentra al menos un objeto, False si no se encuentran coincidencias.
+    """
+    if os.path.exists(archivo):
+        with open(archivo, "rb") as arc:  # Abrir en modo lectura binaria
+            try:
+                clientes = pickle.load(arc)
+                for i in clientes:
+                    if getattr(i, parametro) == coincidencia:
+                        return True  # Se encontró al menos una coincidencia
+            except EOFError:
+                return False  # El archivo está vacío o tiene un formato incorrecto
+    return False  # No se encontraron coincidencias o el archivo no existe
